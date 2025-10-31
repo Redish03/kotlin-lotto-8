4554d16ef2596c4260472d1331514d25bc688a76
Redish03 <pluto0303@pukyong.ac.kr>
2025-10-31 18:18:45 +0900

    feat(InputView.kt): 사용자에게서 입력을 받는다
    - 구매할 금액을 입력받는다
    - 당첨 번호를 입력받는다
    - 보너스 번호를 입력받는다

---
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -0,0 +1,9 @@
+package lotto.view
+
+import camp.nextstep.edu.missionutils.Console
+
+object InputView {
+    fun inputPurchaseMoney() = Console.readLine()
+    fun inputWinNumber() = Console.readLine()
+    fun inputBonusNumber() = Console.readLine()
+}
