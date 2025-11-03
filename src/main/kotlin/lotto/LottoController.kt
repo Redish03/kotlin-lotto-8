2dc4e43c9f98b9ec43d08462b2e77f355a76d857
Redish03 <pluto0303@pukyong.ac.kr>
2025-11-03 16:40:20 +0900

    refactor(LottoController.kt): 함수 분리

--- src/main/kotlin/lotto/LottoController.kt
+++ src/main/kotlin/lotto/LottoController.kt
@@ -3,16 +3,33 @@ package lotto
 import lotto.domain.Parser
 import lotto.view.InputValidator
 import lotto.view.InputView
+import lotto.view.InputView.inputBonusNumber
 
 class LottoController(private val inputView: InputView) {
     private val inputValidator = InputValidator()
-    fun inputFromUser() {
-        val purchaseMoney = inputView.inputPurchaseMoney()
-        inputValidator.validatePurchaseMoney(purchaseMoney)
+
+    fun run() {
+        val purchaseMoney = inputPurchasedMoney()
+        val winningNumbers = inputWinningNumbers()
+        val bonusNumber = inputBonusNumber()
+    }
+
+    private fun inputPurchasedMoney(): Int {
+        val purchasedMoney = inputView.inputPurchaseMoney()
+        inputValidator.validatePurchaseMoney(purchasedMoney)
+        return purchasedMoney.toInt()
+    }
+
+    private fun inputWinningNumbers(): List<Int> {
         val winningNumbers = inputView.inputWinNumber()
         val parsedWinningNumbers = Parser().parse(winningNumbers)
         inputValidator.validateWinningNumbers(parsedWinningNumbers)
+        return parsedWinningNumbers.map { winningNumber -> winningNumber.toInt() }
+    }
+
+    private fun inputBonusNumber(): Int {
         val bonusNumber = inputView.inputBonusNumber()
         inputValidator.validateBonusNumber(bonusNumber)
+        return bonusNumber.toInt()
     }
 }
