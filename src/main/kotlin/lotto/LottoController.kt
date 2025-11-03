d1e96c06df7c0af8baf280e0f04c61d756f8d037
Redish03 <pluto0303@pukyong.ac.kr>
2025-11-03 20:15:40 +0900

    feat(LottoController.kt): 잘못된 입력값이 들어오면 다시 입력하도록 한다

--- src/main/kotlin/lotto/LottoController.kt
+++ src/main/kotlin/lotto/LottoController.kt
@@ -17,20 +17,76 @@ class LottoController(
 ) {
     private val inputValidator = InputValidator()
     private val lottos = mutableListOf<Lotto>()
+    private var purchaseMoney: Int = 0
+    private var winningNumbers: List<Int> = listOf()
+    private var bonusNumber: Int = 0
+
 
     fun run() {
-        val purchaseMoney = inputPurchasedMoney()
-        val lottoCount = Calculator().calculatePurchasedLotto(purchaseMoney)
-        generateLotto(lottoCount)
-        val winningNumbers = inputWinningNumbers()
-        val bonusNumber = inputBonusNumber()
+        inputPurchasedMoney()
+        val lottoCounter = Calculator().calculatePurchasedLotto(purchaseMoney)
+        generateLotto(lottoCounter)
+        inputWinningNumbers()
+        inputBonusNumber()
         val result = LottoResultChecker(lottos, winningNumbers, bonusNumber).checkResult()
         printResult(result, Calculator().calculateRevenueRate(purchaseMoney, result))
     }
 
-    private fun printResult(result: List<WinningGrades>, rate: Double) {
-        outputView.printWinningStatistics(convertResult(result))
-        outputView.printRevenueRate(rate)
+    private fun inputPurchasedMoney() {
+        outputView.printPurchaseMoneyGuide()
+        while (true) {
+            try {
+                val money = inputView.inputPurchaseMoney()
+                inputValidator.validatePurchaseMoney(money)
+                purchaseMoney = money.toInt()
+                break
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun inputWinningNumbers() {
+        outputView.printInputWinningNumbersGuide()
+        while (true) {
+            try {
+                val inputWinningNumbers = Parser().parse(inputView.inputWinNumber())
+                inputValidator.validateWinningNumbers(inputWinningNumbers)
+                winningNumbers = inputWinningNumbers.map { it.toInt() }
+                break
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun inputBonusNumber() {
+        outputView.printInputBonusNumbersGuide()
+        while(true) {
+            try {
+                val inputBonusNumber = inputView.inputBonusNumber()
+                inputValidator.validateBonusNumber(inputBonusNumber)
+                bonusNumber = inputBonusNumber.toInt()
+                return
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
+    private fun convertResult(results: List<WinningGrades>): List<Int> {
+        val convertedResult = mutableListOf(0, 0, 0, 0, 0)
+        for (result in results) {
+            when (result) {
+                WinningGrades.FIRST_GRADE -> convertedResult[0]++
+                WinningGrades.SECOND_GRADE -> convertedResult[1]++
+                WinningGrades.THIRD_GRADE -> convertedResult[2]++
+                WinningGrades.FOURTH_GRADE -> convertedResult[3]++
+                WinningGrades.FIVE_GRADE -> convertedResult[4]++
+                WinningGrades.DIDNT_GRADE -> continue
+            }
+        }
+        return convertedResult
     }
 
     private fun generateLotto(lottoCount: Int) {
@@ -42,40 +98,8 @@ class LottoController(
         }
     }
 
-    private fun inputPurchasedMoney(): Int {
-        outputView.printPurchaseMoneyGuide()
-        val purchasedMoney = inputView.inputPurchaseMoney()
-        inputValidator.validatePurchaseMoney(purchasedMoney)
-        return purchasedMoney.toInt()
-    }
-
-    private fun inputWinningNumbers(): List<Int> {
-        outputView.printInputWinningNumbersGuide()
-        val winningNumbers = inputView.inputWinNumber()
-        val parsedWinningNumbers = Parser().parse(winningNumbers)
-        inputValidator.validateWinningNumbers(parsedWinningNumbers)
-        return parsedWinningNumbers.map { winningNumber -> winningNumber.toInt() }
-    }
-
-    private fun inputBonusNumber(): Int {
-        outputView.printInputBonusNumbersGuide()
-        val bonusNumber = inputView.inputBonusNumber()
-        inputValidator.validateBonusNumber(bonusNumber)
-        return bonusNumber.toInt()
-    }
-
-    private fun convertResult(results: List<WinningGrades>): List<Int> {
-        val convertedResult = mutableListOf(0, 0, 0, 0, 0)
-        for(result in results) {
-            when (result) {
-                WinningGrades.FIRST_GRADE -> convertedResult[0]++
-                WinningGrades.SECOND_GRADE -> convertedResult[1]++
-                WinningGrades.THIRD_GRADE -> convertedResult[2]++
-                WinningGrades.FOURTH_GRADE -> convertedResult[3]++
-                WinningGrades.FIVE_GRADE -> convertedResult[4]++
-                WinningGrades.DIDNT_GRADE -> continue
-            }
-        }
-        return convertedResult
+    private fun printResult(result: List<WinningGrades>, rate: Double) {
+        outputView.printWinningStatistics(convertResult(result))
+        outputView.printRevenueRate(rate)
     }
 }
