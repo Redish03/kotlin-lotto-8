839748840b85c383cc115c13de8f1a9661df389d
Redish03 <pluto0303@pukyong.ac.kr>
2025-11-03 23:27:03 +0900

    test(LottoResultChecker.kt): 로또 보너스 번호를 확인한다

---
+++ src/test/kotlin/lotto/LottoResultCheckerTest.kt
@@ -0,0 +1,32 @@
+package lotto
+
+import lotto.domain.LottoResultChecker
+import lotto.domain.WinningGrades
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class LottoResultCheckerTest {
+    @Test
+    fun `보너스 번호가 다르다면 3등 객체를 반환한다`() {
+        val lotto = Lotto(listOf<Int>(1, 2, 3, 4, 5, 6))
+        val testWinningNumbers = listOf(1, 2, 3, 4, 5, 7)
+        val bonusNumber = 7
+
+        assertEquals(
+            WinningGrades.THIRD_GRADE,
+            LottoResultChecker(listOf(lotto), testWinningNumbers, 7).checkBonusNumber(lotto, bonusNumber)
+        )
+    }
+
+    @Test
+    fun `보너스 번호가 같다면 2등 객체를 반환한다`() {
+        val lotto = Lotto(listOf<Int>(1, 2, 3, 4, 5, 10))
+        val testWinningNumbers = listOf(1, 2, 3, 4, 5, 7)
+        val bonusNumber = 10
+
+        assertEquals(
+            WinningGrades.SECOND_GRADE,
+            LottoResultChecker(listOf(lotto), testWinningNumbers, 7).checkBonusNumber(lotto, bonusNumber)
+        )
+    }
+}
