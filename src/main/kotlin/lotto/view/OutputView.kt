1d9f38419b1a84607b315495b3cac09cf815dd64
Redish03 <pluto0303@pukyong.ac.kr>
2025-11-03 19:16:59 +0900

    refactor(OutputView.kt): Map<String, Int> 형 대신 List<Int>형으로 수정

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -24,17 +24,17 @@ class OutputView {
         println("보너스 번호를 입력해 주세요.")
     }
 
-    fun printWinningStatistics(winningStatistics: Map<String, Int>) {
+    fun printWinningStatistics(winningStatistics: List<Int>) {
         println()
         println("당첨 통계\n---")
-        println("3개 일치 (5,000원) ${winningStatistics["fifthCount"]}개")
-        println("4개 일치 (50,000원) ${winningStatistics["fourthCount"]}개")
-        println("5개 일치 (1,500,000원) ${winningStatistics["thirdCount"]}개")
-        println("5개 일치, 보너스 볼 일치 (30,000,000원) ${winningStatistics["secondCount"]}개")
-        println("6개 일치 (2,000,000,000원) ${winningStatistics["firstCount"]}개")
+        println("3개 일치 (5,000원) - ${winningStatistics[4]}개")
+        println("4개 일치 (50,000원) - ${winningStatistics[3]}개")
+        println("5개 일치 (1,500,000원) - ${winningStatistics[2]}개")
+        println("5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningStatistics[1]}개")
+        println("6개 일치 (2,000,000,000원) - ${winningStatistics[0]}개")
     }
 
     fun printRevenueRate(revenueRate: Double) {
-        println("총 수익률은 ${revenueRate}입니다.")
+        println("총 수익률은 ${revenueRate}%입니다.")
     }
 }
