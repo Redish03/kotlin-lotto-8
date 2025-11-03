039021b5b9ec438472c719eabb3bf1aa0ffff0b6
Redish03 <pluto0303@pukyong.ac.kr>
2025-11-03 16:21:44 +0900

    feat(Calculator.kt): 계산기 구현
    - 구매한 로또 개수를 계산
    - 로또의 수익률 계산

---
+++ src/main/kotlin/lotto/domain/Calculator.kt
@@ -0,0 +1,32 @@
+package lotto.domain
+
+import kotlin.math.round
+
+class Calculator {
+    fun calculatePurchasedLotto(purchasedMoney: Int) = purchasedMoney.div(ONE_LOTTO_MONEY)
+    fun calculateRevenueRate(purchasedMoney: Int, lottoResults: List<WinningGrades>): Double {
+        var revenue = 0.0
+        for (lottoResult in lottoResults) {
+            revenue += when (lottoResult) {
+                WinningGrades.FIRST_GRADE -> FIRST_GRADE_MONEY
+                WinningGrades.SECOND_GRADE -> SECOND_GRADE_MONEY
+                WinningGrades.THIRD_GRADE -> THIRD_GRADE_MONEY
+                WinningGrades.FOURTH_GRADE -> FOURTH_GRADE_MONEY
+                WinningGrades.FIVE_GRADE -> FIFTH_GRADE_MONEY
+                WinningGrades.DIDNT_GRADE -> DIDNT_GRADED_MONEY
+            }
+        }
+        val rate = revenue * 100 / purchasedMoney
+        return round(rate * 10) / 10
+    }
+
+    companion object {
+        private const val ONE_LOTTO_MONEY = 1000
+        private const val FIRST_GRADE_MONEY = 2000000000
+        private const val SECOND_GRADE_MONEY = 30000000
+        private const val THIRD_GRADE_MONEY = 1500000
+        private const val FOURTH_GRADE_MONEY = 50000
+        private const val FIFTH_GRADE_MONEY = 5000
+        private const val DIDNT_GRADED_MONEY = 0
+    }
+}
