abc947235c988fee65cce2acafc1ab6e9a5aa9d0
Redish03 <pluto0303@pukyong.ac.kr>
2025-11-03 17:28:32 +0900

    feat(OutputView.kt): 결과를 출력한다

--- src/main/kotlin/lotto/LottoController.kt
+++ src/main/kotlin/lotto/LottoController.kt
@@ -1,36 +1,47 @@
 package lotto
 
+import lotto.domain.Calculator
 import lotto.domain.Parser
 import lotto.domain.numbergenerator.NumberGenerator
 import lotto.view.InputValidator
 import lotto.view.InputView
 import lotto.view.InputView.inputBonusNumber
+import lotto.view.OutputView
 
-class LottoController(private val inputView: InputView, private val numberGenerator: NumberGenerator) {
+class LottoController(
+    private val inputView: InputView,
+    private val outputView: OutputView,
+    private val numberGenerator: NumberGenerator
+) {
     private val inputValidator = InputValidator()
     private val lottos = mutableListOf<Lotto>()
 
     fun run() {
         val purchaseMoney = inputPurchasedMoney()
+        val lottoCount = Calculator().calculatePurchasedLotto(purchaseMoney)
+        generateLotto(lottoCount)
         val winningNumbers = inputWinningNumbers()
         val bonusNumber = inputBonusNumber()
     }
 
     private fun generateLotto(lottoCount: Int) {
+        outputView.printPurchasedLottoCounts(lottoCount)
         repeat(lottoCount) {
-            val generatedNumber = numberGenerator.generateNumber()
-            // TODO: 생성한 번호 출력
+            val generatedNumber = numberGenerator.generateNumber().sorted()
+            outputView.printPurchasedLottoNumbers(generatedNumber)
             lottos.add(Lotto(generatedNumber))
         }
     }
 
     private fun inputPurchasedMoney(): Int {
+        outputView.printPurchaseMoneyGuide()
         val purchasedMoney = inputView.inputPurchaseMoney()
         inputValidator.validatePurchaseMoney(purchasedMoney)
         return purchasedMoney.toInt()
     }
 
     private fun inputWinningNumbers(): List<Int> {
+        outputView.printInputWinningNumbersGuide()
         val winningNumbers = inputView.inputWinNumber()
         val parsedWinningNumbers = Parser().parse(winningNumbers)
         inputValidator.validateWinningNumbers(parsedWinningNumbers)
@@ -38,6 +49,7 @@ class LottoController(private val inputView: InputView, private val numberGenera
     }
 
     private fun inputBonusNumber(): Int {
+        outputView.printInputBonusNumbersGuide()
         val bonusNumber = inputView.inputBonusNumber()
         inputValidator.validateBonusNumber(bonusNumber)
         return bonusNumber.toInt()
