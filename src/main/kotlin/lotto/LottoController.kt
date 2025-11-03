a7ba30aeb828a1d476efcae988847fc2001db2f1
Redish03 <pluto0303@pukyong.ac.kr>
2025-11-03 16:56:34 +0900

    feat(LottoController.kt): 로또들을 생성한다

--- src/main/kotlin/lotto/LottoController.kt
+++ src/main/kotlin/lotto/LottoController.kt
@@ -1,17 +1,27 @@
 package lotto
 
 import lotto.domain.Parser
+import lotto.domain.numbergenerator.NumberGenerator
 import lotto.view.InputValidator
 import lotto.view.InputView
 import lotto.view.InputView.inputBonusNumber
 
-class LottoController(private val inputView: InputView) {
+class LottoController(private val inputView: InputView, private val numberGenerator: NumberGenerator) {
     private val inputValidator = InputValidator()
+    private val lottos = mutableListOf<Lotto>()
 
     fun run() {
         val purchaseMoney = inputPurchasedMoney()
         val winningNumbers = inputWinningNumbers()
         val bonusNumber = inputBonusNumber()
+    }
+
+    private fun generateLotto(lottoCount: Int) {
+        repeat(lottoCount) {
+            val generatedNumber = numberGenerator.generateNumber()
+            // TODO: 생성한 번호 출력
+            lottos.add(Lotto(generatedNumber))
+        }
     }
 
     private fun inputPurchasedMoney(): Int {
